49746b727ee0df593b59ce314f1e1e3f19eb2216
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2021-01-24 12:16:12 -0300

    Criando estrutura basica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ProductFormComponent } from './product-form/product-form.component';
+import { DepartmentFormComponent } from './department-form/department-form.component';
+import { ProductsTableComponent } from './products-table/products-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductFormComponent,
+    DepartmentFormComponent,
+    ProductsTableComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
